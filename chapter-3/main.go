20845dec5ad8f7b6e60c1bcc73225ea1ab38f094
mengyuwang <mengyu.wang.0124@gmail.com>
2023-01-18 14:31:06 +0800

    add utils func

--- chapter-3/main.go
+++ chapter-3/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/Jeffail/tunny"
+	"github.com/wangmengyu/coding-576/utils"
 )
 
 func main() {
 	fmt.Println("hello world")
 	pool := tunny.Pool{}
 	fmt.Printf("%+v, %T", pool, pool)
+	val := utils.GetCurrentDate()
+	fmt.Println("current date: ", val)
 }
