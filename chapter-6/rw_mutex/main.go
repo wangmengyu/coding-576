eed6a81478f4e3a1463ef41667e2e23fe85f685b
mengyu wang <mengyu.wang.0124@gmail.com>
2023-01-22 16:07:06 +0800

    sync mutex

---
+++ chapter-6/rw_mutex/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Person struct {
+	mu     sync.RWMutex // 读写锁
+	salary int          // 工资
+	level  int          // 等级
+}
+
+// promote 升值加薪
+func (p *Person) promote() {
+	p.mu.Lock()         // 操作之前加锁
+	defer p.mu.Unlock() // 习惯在lock之后加defer的解锁语句. 防止过程中出现panic.
+	p.salary = p.salary + 1000
+	p.level = p.level + 1
+
+	fmt.Println(p.salary)
+	fmt.Println(p.level)
+	//p.mu.Unlock() //  这里就不需要Unlock 了, 因为defer 会解锁.
+}
+
+// 打印个人信息, [纯读取]
+func (p *Person) printPerson() {
+	p.mu.RLock()         // 读取锁
+	defer p.mu.RUnlock() //读取解锁
+	fmt.Println(p.salary)
+	fmt.Println(p.level)
+}
+
+func main() {
+	p := Person{
+		salary: 10000,
+		level:  1,
+	}
+
+	go p.printPerson() //  并发读. 读取锁
+	go p.printPerson() //  并发读. 读取锁
+	go p.printPerson() //  并发读 . 读取锁
+	go p.promote()     // 会等前面三个都做完才做, 用了互斥锁
+	time.Sleep(1 * time.Second)
+
+}
