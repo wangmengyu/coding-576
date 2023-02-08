37ddaaf918521e8e0c1b872ba9355295f04d0fd9
mengyuwang <mengyu.wang.0124@gmail.com>
2023-02-08 17:19:48 +0800

    add comment

--- net/tcp_gouroutine_per_connection/main.go
+++ net/tcp_gouroutine_per_connection/main.go
@@ -4,54 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
 	"net"
 )
 
-// ListenAndServe 每一个链接一个goroutine.
-func ListenAndServe(address string) {
-	listen, err := net.Listen("tcp", address)
-	defer listen.Close() // 每次listen好, 就接defer关闭.
+func ListenAndServe(addr string) {
+
+	listen, err := net.Listen("tcp", addr)
+	defer listen.Close()
 	if err != nil {
-		// 碰到错误直接退出
-		log.Fatal(err.Error())
+		panic(err.Error())
 	}
 
-	// 不断接受新的请求, 创建出新的请求连接出来
+	// 为每个请求设置自己的goroutie
 	for {
+		// 持续接收请求
 		conn, err := listen.Accept()
 		if err != nil {
-			log.Fatal("accept err:", err.Error())
+			fmt.Println("accept err:", err.Error())
+			continue
 		}
-		go Handle(conn)
+
+		// 让每个链接在自己的协程里工作
+		go handler(conn)
 	}
+
 }
 
-// Handle 处理每个通道的读写
-func Handle(conn net.Conn) {
-	reader := bufio.NewReader(conn) // 使用bufio标准库提供的缓冲区功能充当读取器
+func handler(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
 	for {
-		// ReadString 会一直阻塞直到遇到分隔符 '\n'
-		// 遇到分隔符后会返回上次遇到分隔符或连接建立后收到的所有数据, 包括分隔符本身
-		// 若在遇到分隔符之前遇到异常, ReadString 会返回已收到的数据和错误信息
-		msg, err := reader.ReadString('\n') //
+		// 持续处理读写操作
+		msg, err := reader.ReadString('\n')
 		if err != nil {
-			// 错误分为中断连接时候类型是io.EOF
 			if err == io.EOF {
-				log.Println("connection close")
+				fmt.Println("conn closed")
 			} else {
-				log.Println("read msg err: ", err.Error())
+				fmt.Println("read err:", err.Error())
 			}
-			return // 发生错误必须返回了
-		}
-		b := []byte(msg) // 强制转成[]byte类型
-		fmt.Println("read msg:", string(b))
-		_, err = conn.Write(b) // 将接收到的数据写回到连接中
-		if err != nil {
-			log.Println("write err:", err.Error())
 			return
 		}
+		// 将数据写回通道
+		_, err = conn.Write([]byte(msg))
+		if err != nil {
+			fmt.Println("write err", err.Error())
+			return
+		}
+
 	}
+
 }
 
 func main() {
