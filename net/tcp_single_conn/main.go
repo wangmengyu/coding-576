aaff9a5582dcc25ee8e667f0021d1b1f7dad0648
mengyuwang <mengyu.wang.0124@gmail.com>
2023-02-08 16:19:09 +0800

    add comment

--- net/tcp_single_conn/main.go
+++ net/tcp_single_conn/main.go
@@ -10,32 +10,33 @@ func main() {
 	// 主要还是看tcp_goroutine_per_connection目录下的实现
 	// 监听本地的8888端口 , 等待新的连接进来
 	listen, err := net.Listen("tcp", ":8888")
-	// listen的值是一个listen状态的socket.
-	if err != nil {
-		panic(err.Error())
-	}
-	conn, err := listen.Accept() // 接收新连接
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// 新建缓存变量接收数据
+	conn, err := listen.Accept()
+	if err != nil {
+
+	}
+
 	var body [100]byte
-	// 以下这种方式一旦客户端断开连接. server就退出了.
 	for {
-		// 不断接受数据
-		_, err = conn.Read(body[:])
+		// 不断尝试获得一个数据
+		_, err = conn.Read(body[:]) // 读取到body 里
 		if err != nil {
-			fmt.Println("read err:", err.Error())
-			break // 接收失败退出本次循环
+			fmt.Println("read err: ", err.Error())
+			// 读取发生错误立即退出循环
+			break
 		}
-		// 成功接收到数据 , 发回数据给客户端.
-		fmt.Printf("收到数据: %s\n", body[:])
-		_, err = conn.Write(body[:])
+
+		_, err = conn.Write(body[:]) // 把数据写回通道
 		if err != nil {
+			// 写发生错误 . 也立即退出
+
 			fmt.Println("write err:", err.Error())
 			break
 		}
+
 	}
 
 }
